014687cf233141420a34e40a52cca95866249f6c
Anurag <sagar@gmail.com>
2023-04-04 03:51:28 +0000

    Devops changes commit

--- demo.java
+++ demo.java
@@ -2,5 +2,8 @@ public class Demo{
 	public static void main(String args[]){
 		// print statement for hello world
 		System.out.println("Hello World")
+			// print statement for Devops
+		System.out.println("Welcome to devops")
+
 	}
 }
